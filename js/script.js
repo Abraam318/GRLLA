fb5fe70eec72823e055ea5d1a4c720fd3a2265d0
Abraam318 <abraamadel0@gmail.com>
2025-12-25 17:19:15 +0200

    Add captain carousel to About Us section and Coming Soon clothing section

--- js/script.js
+++ js/script.js
@@ -93,7 +93,7 @@ function toggleMobileMenu() {
 
 // Success Stories Slider
 const successImages = [
-    '1-1.png',  '1-3.png', '1-7.png', '1-8.png'
+    '1-1.png','1-2.png',  '1-3.png', '1-4.png', '1-5.png', '1-6.png', '1-7.png', '1-8.png'
 ];
 
 function initSuccessSlider() {
